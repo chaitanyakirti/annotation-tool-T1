1f2a2be3526fad302b646676e4bdc09186e82603
choudhury528 <moumitachoudhury2312gmail.com>
2022-07-20 03:37:11 +0530

    Dashboard added

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
-   return render(request , 'index.html')
+    return render(request, 'index.html')
+
 
 def Signup(request):
-   return render(request, 'signup.html')
+    if request.method == "POST":
+        username = request.POST['fullName']
+        email = request.POST['email']
+        role = request.POST['role']
+        password = request.POST['password']
+
+        myuser = User.objects.create_user(username, email, password)
+        myuser.role = role
+
+        myuser.save()
+
+        messages.success(request, " Your account has been successfully created")
+
+        return redirect('Login')
+    return render(request, 'signup.html')
+
 
 def Login(request):
-   return render(request, 'login.html')
-
+    return render(request, 'Login.html')
